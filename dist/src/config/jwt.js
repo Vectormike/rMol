5a52a325e05f3657650e39a042c7dafd30015021
Victor Jonah <victorjonah199@gmail.com>
2022-02-18 08:55:13 +0100

    ...

--- dist/src/config/jwt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-var jwtConfig = {
-    appKey: process.env.APP_SECRET_KEY,
-    refreshTokenKey: process.env.REFRESH_SECRET_KEY,
-};
-exports.default = jwtConfig;
-//# sourceMappingURL=jwt.js.map
